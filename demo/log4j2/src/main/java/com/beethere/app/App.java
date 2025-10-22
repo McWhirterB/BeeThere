9b6f480fc96379a659766c734b7fc2660f2dc1cb
dhines <dhines@rocketsoftware.com>
2025-10-22 17:23:55 -0400

    CHANGED: log4j2 demo now working

--- demo/log4j2/src/main/java/com/beethere/app/App.java
+++ demo/log4j2/src/main/java/com/beethere/app/App.java
@@ -1,20 +1,30 @@
+
 package com.beethere.app;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-/**
- * Hello world!
- */
+
 public class App {
+
     private static final Logger logger = LogManager.getLogger(App.class);
 
-     public static void main( String[] args ) {
-        System.out.println( "Hello, user!" );
-        logger.trace("We've just greeted the user!");
-        logger.debug("We've just greeted the user!");
-        logger.info("We've just greeted the user!");
-        logger.warn("We've just greeted the user!");
-        logger.error("We've just greeted the user!");
-        logger.fatal("We've just greeted the user!");
+    public static void main(String[] args) {
+
+        logger.debug("Hello from Log4j 2");
+
+        // in old days, we need to check the log level to increase performance
+        /*if (logger.isDebugEnabled()) {
+            logger.debug("{}", getNumber());
+        }*/
+
+        // with Java 8, we can do this, no need to check the log level
+        logger.debug("{}", () -> getNumber());
+
     }
+
+    static int getNumber() {
+        return 5;
+    }
+
 }
