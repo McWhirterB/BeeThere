bc4ca6d1110cebe28ae117bc500ae5654fb23b4d
Rocket Student <noone@nowhere.com>
2025-10-29 12:14:17 -0400

    added some validation on reservation, need a little more for the owner field though

--- src/src/main/java/com/beethere/model/Reservation.java
+++ src/src/main/java/com/beethere/model/Reservation.java
@@ -25,37 +25,101 @@ public class Reservation {
     public int getID() {
         return ID;
     }
+
+
     public void setID(int ID) {
+        //LOG.debug("Setting the reservation id");
+        if (ID < 0){
+            //LOG.error("Reservation ID cannot be less than 0");
+            throw new IllegalArgumentException("Reservation ID cannot be less than 0");
+        }
+
         this.ID = ID;
     }
+
+    
     public Date getStartTime() {
         return startTime;
     }
+
+
     public void setStartTime(Date startTime) {
+        Date now = new Date();
+        if (startTime == null){
+            throw new IllegalArgumentException("Start time cannot be null");
+        }
+        if (startTime.after(endTime)) {
+            throw new IllegalArgumentException("Start time cannot be after end time");
+        }
+        if (startTime.equals(endTime)) {
+            throw new IllegalArgumentException("Start time cannot be the same as end time");
+        }
+        if (startTime.before(now)) {
+            throw new IllegalArgumentException("Start time cannot be in the past");
+        }
         this.startTime = startTime;
     }
+
+
     public Date getEndTime() {
         return endTime;
     }
+
+
     public void setEndTime(Date endTime) {
+        Date now = new Date();
+
+        if (endTime == null){
+            throw new IllegalArgumentException("End time cannot be null");
+        }
+        if (endTime.before(startTime)) {
+            throw new IllegalArgumentException("End time cannot be before start time");
+        }
+        if (endTime.equals(startTime)) {
+            throw new IllegalArgumentException("End time cannot be the same as start time");
+        }
+        if (endTime.before(now)) {
+            throw new IllegalArgumentException("End time cannot be in the past");
+        }
         this.endTime = endTime;
     }
+
+
     public String getOwner() {
         return owner;
     }
+    
+    
     public void setOwner(String owner) {
+        if (owner == null || owner.isEmpty()){
+            throw new IllegalArgumentException("Owner cannot be null or empty");
+        }
         this.owner = owner;
     }
+
+
     public int getOwnerID() {
         return ownerID;
     }
+    
+    
     public void setOwnerID(int ownerID) {
+        if (ownerID < 0){
+            throw new IllegalArgumentException("Owner ID cannot be less than 0");
+        }
         this.ownerID = ownerID;
     }
+
+
     public ArrayList<Room> getRooms() {
         return rooms;
     }
+
+
     public void setRooms(ArrayList<Room> rooms) {
+        if (rooms == null || rooms.isEmpty()){
+            throw new IllegalArgumentException("Rooms cannot be null or empty");
+        }
         this.rooms = rooms;
     }
 
