09b4e874e17f448d7ac82d43113da16da758b9d5
dward <dward@rocketsoftware.com>
2025-11-03 16:41:57 -0500

    calling auth server in controller

--- src/src/main/java/com/beethere/controller/RoomController.java
+++ src/src/main/java/com/beethere/controller/RoomController.java
@@ -1,7 +1,6 @@
-package com.beethere;
+package com.beethere.controller;
 
 import com.beethere.service.*;
-
 import com.beethere.model.*;
 
 import java.util.*;
@@ -14,15 +13,29 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 @RestController
 @RequestMapping("/room")
 public class RoomController {
+	private AuthProxy authProxy;
 	private RoomService roomService;
 
-	public RoomController(RoomService roomService) {
+	public RoomController(AuthProxy authProxy, RoomService roomService) {
+		this.authProxy = authProxy;
 		this.roomService = roomService;
 	}
 
 	@GetMapping("/")
-	public ResponseEntity<?> testEndpoint() {
+	public ResponseEntity<?> testEndpoint(@RequestHeader(value = "Bearer", required = false) String token) {
+		if (token == null || token.isEmpty()) {
+			return new ResponseEntity<String>("Token Required", HttpStatus.BAD_REQUEST);
+		}
+		Employee e = null;
+		
+		try { 
+			e = authProxy.verifyEmployee(new TokenRequest(token));	
+		} catch (Exception ex) {
+			return new ResponseEntity<String>("Failed to validate token", HttpStatus.UNAUTHORIZED);
+		}
 		Iterable<Room> rooms = roomService.getRooms();
 		return new ResponseEntity<Iterable<Room>>(rooms, HttpStatus.ACCEPTED);
+		//Employee e = authProxy.verifyEmployee(new TokenRequest(token));
+		//return new ResponseEntity<Employee>(e, HttpStatus.ACCEPTED);
 	}
 }
