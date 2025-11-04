a8ee615ca3c96648d07cdb0da1ea9f751c6283cd
dhines <dhines@rocketsoftware.com>
2025-11-04 15:27:11 -0600

    CHANGED: AppApplicationRoute

--- src/src/main/java/com/beethere/app/AppApplication.java
+++ src/src/main/java/com/beethere/app/AppApplication.java
@@ -2,9 +2,11 @@ package com.beethere.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 
 @SpringBootApplication
+@ComponentScan(basePackages="com.beethere")
 public class AppApplication {
 
 	public static void main(String[] args) {
