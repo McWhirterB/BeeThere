26f2e99d4090fb6a05b7c69f70d95a303de2a77f
dward <dward@rocketsoftware.com>
2025-11-05 16:51:00 -0500

    getter setter logic for reservation

--- src/src/main/java/com/beethere/model/Reservation.java
+++ src/src/main/java/com/beethere/model/Reservation.java
@@ -12,11 +12,25 @@ public class Reservation {
 	public Date startTime;
 	public Date endTime;
 
+    public Reservation() {}
+
+    public Reservation(Integer reservationId, Integer employeId, String employeeName, Date startTime, Date endTime) {
+        setReservationId(reservationId);
+        setEmployeeId(employeId);
+        setEmployeeName(employeeName);
+        setStartTime(startTime);
+        setEndTime(endTime);
+    }
+
     public Integer getReservationId() {
         return reservationId;
     }
 
     public void setReservationId(Integer reservationId) {
+        if (reservationId < 0){
+            //LOG.error("Reservation ID cannot be less than 0");
+            throw new IllegalArgumentException("Reservation ID cannot be less than 0");
+        }
         this.reservationId = reservationId;
     }
 
@@ -41,6 +55,19 @@ public class Reservation {
     }
 
     public void setStartTime(Date startTime) {
+        Date now = new Date();
+        if (startTime == null){
+            throw new IllegalArgumentException("Start time cannot be null");
+        }
+        if (startTime.after(endTime)) {
+            throw new IllegalArgumentException("Start time cannot be after end time");
+        }
+        if (startTime.equals(endTime)) {
+            throw new IllegalArgumentException("Start time cannot be the same as end time");
+        }
+        if (startTime.before(now)) {
+            throw new IllegalArgumentException("Start time cannot be in the past");
+        }
         this.startTime = startTime;
     }
 
@@ -49,6 +76,20 @@ public class Reservation {
     }
 
     public void setEndTime(Date endTime) {
+        Date now = new Date();
+
+        if (endTime == null){
+            throw new IllegalArgumentException("End time cannot be null");
+        }
+        if (endTime.before(startTime)) {
+            throw new IllegalArgumentException("End time cannot be before start time");
+        }
+        if (endTime.equals(startTime)) {
+            throw new IllegalArgumentException("End time cannot be the same as start time");
+        }
+        if (endTime.before(now)) {
+            throw new IllegalArgumentException("End time cannot be in the past");
+        }
         this.endTime = endTime;
     }
 }
