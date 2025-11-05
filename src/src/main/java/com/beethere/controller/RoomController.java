2d7dd9159d656e303309c720732caf4b70c67e8f
Rocket Student <noone@nowhere.com>
2025-11-05 12:17:05 -0500

    working

--- src/src/main/java/com/beethere/controller/RoomController.java
+++ src/src/main/java/com/beethere/controller/RoomController.java
@@ -1,16 +1,24 @@
 package com.beethere.controller;
 
-import com.beethere.service.RoomService;
-import com.beethere.model.Room;
-import com.beethere.model.Employee;
-import com.beethere.model.TokenRequest;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.Optional;
 import java.util.function.Supplier;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.beethere.model.Employee;
+import com.beethere.model.Room;
+import com.beethere.model.TokenRequest;
+import com.beethere.service.RoomService;
 
 @RestController
 @RequestMapping("/room")
@@ -32,8 +40,12 @@ public class RoomController {
 
         try {
             Employee employee = authProxy.verifyEmployee(new TokenRequest(token));
+            employee.toStringFormat();
+            //String response = authProxy.verifyEmployee(new TokenRequest(token));
+            //System.out.println(response);
             return action.get();
         } catch (Exception ex) {
+            ex.printStackTrace();
             return new ResponseEntity<>("Failed to validate token", HttpStatus.UNAUTHORIZED);
         }
     }
--- src/src/main/java/com/beethere/model/Employee.java
+++ src/src/main/java/com/beethere/model/Employee.java
@@ -4,160 +4,154 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.EntityManager;
 import jakarta.persistence.Id;
 
 @Entity
 public class Employee {
-	@Id
-	private Integer ID;
-	private String fName;	
-	private String lName;
-	private String loc;
-	private String dept;
-	private String title;
 
+    @Id
+    private Integer id;
+    private String fName;
+    private String lName;
+    private String loc;
+    private String dept;
+    private String title;
 
     private static final Logger LOG = LogManager.getLogger();
 
-    public Employee(int id, String location, String firstName, String lastName, String department, String title) {
+    public Employee(int id, String loc, String fName, String lName, String dept, String title) {
         setId(id);
-        setLocation(location);
-        setFirstName(firstName);
-        setLastName(lastName);
-        setDepartment(department);
+        setLoc(loc);
+        setFName(fName);
+        setLName(lName);
+        setDept(dept);
         setTitle(title);
     }
 
-	//TODO: Implement data validation for setters
+    public void toStringFormat(){
+        System.out.println("Employee ID: " + getId());
+        System.out.println("Location: " + getLoc());
+        System.out.println("First Name: " + getFName());
+        System.out.println("Last Name: " + getLName());
+        System.out.println("Department: " + getDept());
+        System.out.println("Title: " + getTitle());
+    }
+
+    // Getters and Setters
     public int getId() {
         LOG.debug("Getting the employee id");
-        return ID;
+        return id;
     }
-    
-    
+
     public void setId(int id) {
         LOG.debug("Setting the employee id");
-
-        if (id < 0){
-
+        if (id < 0) {
             LOG.error("Employee ID cannot be less than 0");
             throw new IllegalArgumentException("Employee ID cannot be less than 0");
         }
-
         LOG.debug("Employee ID set to " + id);
-        this.ID = id;
+        this.id = id;
     }
 
-
-   public String getLocation() {
+    public String getLoc() {
         LOG.debug("Getting the employee location");
         return loc;
     }
 
-
-    public void setLocation(String location) {
+    public void setLoc(String loc) {
         LOG.debug("Setting the employee location");
         final int min = 2;
         final int max = 20;
 
-        if (location == null || location.isEmpty()){
-            LOG.error("Location can not be null or empty");
+        if (loc == null || loc.isEmpty()) {
+            LOG.error("Location cannot be null or empty");
             throw new IllegalArgumentException("Location cannot be null or empty");
         }
-        if (location.length() < min || location.length() > max) {
-            LOG.error("Location must be between " + min + " and " + max + "characters");
+        if (loc.length() < min || loc.length() > max) {
+            LOG.error("Location must be between " + min + " and " + max + " characters");
             throw new IllegalArgumentException("Location must be between " + min + " and " + max + " characters");
         }
-        
-        
-        LOG.debug("setting location to " + location);
-        this.loc = location;
+
+        LOG.debug("Setting location to " + loc);
+        this.loc = loc;
     }
 
-    public String getFirstName() {
+    public String getFName() {
         LOG.debug("Getting the employee first name");
         return fName;
     }
 
-
-    public void setFirstName(String firstName) {
+    public void setFName(String fName) {
         LOG.debug("Setting the employee first name");
-
         final int min = 1;
         final int max = 20;
 
-        if (firstName == null || firstName.isEmpty()){
+        if (fName == null || fName.isEmpty()) {
             LOG.error("First name cannot be null or empty");
             throw new IllegalArgumentException("First name cannot be null or empty");
         }
-        if (firstName.length() < min || firstName.length() > max) {
+        if (fName.length() < min || fName.length() > max) {
             LOG.error("First name must be between " + min + " and " + max + " characters");
             throw new IllegalArgumentException("First name must be between " + min + " and " + max + " characters");
         }
-        
-        LOG.debug("Setting first name to " + firstName);
-        this.fName = firstName;
+
+        LOG.debug("Setting first name to " + fName);
+        this.fName = fName;
     }
 
-
-    public String getLastName() {
+    public String getLName() {
         LOG.debug("Getting the employee last name");
         return lName;
     }
 
-
-    public void setLastName(String lastName) {
+    public void setLName(String lName) {
         LOG.debug("Setting the employee last name");
-
         final int min = 1;
         final int max = 20;
 
-        if (lastName == null || lastName.isEmpty()){
+        if (lName == null || lName.isEmpty()) {
             LOG.error("Last name cannot be null or empty");
             throw new IllegalArgumentException("Last name cannot be null or empty");
         }
-        if (lastName.length() < min || lastName.length() > max) {
+        if (lName.length() < min || lName.length() > max) {
             LOG.error("Last name must be between " + min + " and " + max + " characters");
             throw new IllegalArgumentException("Last name must be between " + min + " and " + max + " characters");
         }
-        
 
-        LOG.debug("Setting last name to " + lastName);
-        this.lName = lastName;
+        LOG.debug("Setting last name to " + lName);
+        this.lName = lName;
     }
 
-
-    public String getDepartment() {
+    public String getDept() {
         LOG.debug("Getting the employee department");
         return dept;
     }
 
-
-    public void setDepartment(String department) {
+    public void setDept(String dept) {
         LOG.debug("Setting the employee department");
-
         final int min = 1;
         final int max = 40;
 
-        if (department == null || department.isEmpty()){
+        if (dept == null || dept.isEmpty()) {
             LOG.error("Department cannot be null or empty");
             throw new IllegalArgumentException("Department cannot be null or empty");
-        }   
-        if (department.length() < min || department.length() > max) {
+        }
+        if (dept.length() < min || dept.length() > max) {
             LOG.error("Department must be between " + min + " and " + max + " characters");
             throw new IllegalArgumentException("Department must be between " + min + " and " + max + " characters");
         }
-        
-        LOG.debug("Setting department to " + department);
-        this.dept = department;
+
+        LOG.debug("Setting department to " + dept);
+        this.dept = dept;
     }
+
     public String getTitle() {
+        LOG.debug("Getting the employee title");
         return title;
     }
+
     public void setTitle(String title) {
+        LOG.debug("Setting the employee title");
         this.title = title;
     }
-
 }
-
