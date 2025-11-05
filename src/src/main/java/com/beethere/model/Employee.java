a1da01b1dec92ec1e2c2c44265f34d6dccd2a44e
dhines <dhines@rocketsoftware.com>
2025-11-04 19:25:41 -0500

    CHANGED: Added getters and setters back to Employee and Room

--- src/src/main/java/com/beethere/model/Employee.java
+++ src/src/main/java/com/beethere/model/Employee.java
@@ -7,11 +7,152 @@ import jakarta.persistence.Id;
 @Entity
 public class Employee {
 	@Id
-	public Integer id;
-	public String fName;	
-	public String lName;
-	public String loc;
-	public String dept;
-	public String title;
+	private Integer id;
+	private String fName;	
+	private String lName;
+	private String loc;
+	private String dept;
+	private String title;
+
+
+    public Employee(int id, String location, String firstName, String lastName, String department, Roles role) {
+        setId(id);
+        setLocation(location);
+        setFirstName(firstName);
+        setLastName(lastName);
+        setDepartment(department);
+        setTitle(role);
+    }
+
+	//TODO: Implement data validation for setters
+    public int getId() {
+        LOG.debug("Getting the employee id");
+        return id;
+    }
+    
+    
+    public void setId(int id) {
+        LOG.debug("Setting the employee id");
+
+        if (ID < 0){
+
+            LOG.error("Employee ID cannot be less than 0");
+            throw new IllegalArgumentException("Employee ID cannot be less than 0");
+        }
+
+        LOG.debug("Employee ID set to " + id);
+        this.id = id;
+    }
+
+
+   public String getLocation() {
+        LOG.debug("Getting the employee location");
+        return loc;
+    }
+
+
+    public void setLocation(String location) {
+        LOG.debug("Setting the employee location");
+        final int min = 2;
+        final int max = 20;
+
+        if (location == null || location.isEmpty()){
+            LOG.error("Location can not be null or empty");
+            throw new IllegalArgumentException("Location cannot be null or empty");
+        }
+        if (location.length() < min || location.length() > max) {
+            LOG.error("Location must be between " + min + " and " + max + "characters");
+            throw new IllegalArgumentException("Location must be between " + min + " and " + max + " characters");
+        }
+        
+        
+        LOG.debug("setting location to " + location);
+        this.loc = location;
+    }
+
+    public String getFirstName() {
+        LOG.debug("Getting the employee first name");
+        return fName;
+    }
+
+
+    public void setFirstName(String firstName) {
+        LOG.debug("Setting the employee first name");
+
+        final int min = 1;
+        final int max = 20;
+
+        if (firstName == null || firstName.isEmpty()){
+            LOG.error("First name cannot be null or empty");
+            throw new IllegalArgumentException("First name cannot be null or empty");
+        }
+        if (firstName.length() < min || firstName.length() > max) {
+            LOG.error("First name must be between " + min + " and " + max + " characters");
+            throw new IllegalArgumentException("First name must be between " + min + " and " + max + " characters");
+        }
+        
+        LOG.debug("Setting first name to " + firstName);
+        this.firstName = firstName;
+    }
+
+
+    public String getLastName() {
+        LOG.debug("Getting the employee last name");
+        return lName;
+    }
+
+
+    public void setLastName(String lastName) {
+        LOG.debug("Setting the employee last name");
+
+        final int min = 1;
+        final int max = 20;
+
+        if (lastName == null || lastName.isEmpty()){
+            LOG.error("Last name cannot be null or empty");
+            throw new IllegalArgumentException("Last name cannot be null or empty");
+        }
+        if (lastName.length() < min || lastName.length() > max) {
+            LOG.error("Last name must be between " + min + " and " + max + " characters");
+            throw new IllegalArgumentException("Last name must be between " + min + " and " + max + " characters");
+        }
+        
+
+        LOG.debug("Setting last name to " + lastName);
+        this.lName = lastName;
+    }
+
+
+    public String getDepartment() {
+        LOG.debug("Getting the employee department");
+        return dept;
+    }
+
+
+    public void setDepartment(String department) {
+        LOG.debug("Setting the employee department");
+
+        final int min = 1;
+        final int max = 40;
+
+        if (department == null || department.isEmpty()){
+            LOG.error("Department cannot be null or empty");
+            throw new IllegalArgumentException("Department cannot be null or empty");
+        }   
+        if (department.length() < min || department.length() > max) {
+            LOG.error("Department must be between " + min + " and " + max + " characters");
+            throw new IllegalArgumentException("Department must be between " + min + " and " + max + " characters");
+        }
+        
+        LOG.debug("Setting department to " + department);
+        this.dept = department;
+    }
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
 }
 
--- src/src/main/java/com/beethere/model/Room.java
+++ src/src/main/java/com/beethere/model/Room.java
@@ -8,11 +8,158 @@ import jakarta.persistence.Id;
 @Entity
 public class Room {
 	@Id
-	public Integer roomId;
-	public String location;
-	public String building;
-	public String roomNumber;
-	public String type;
-	public Integer seats;
+	private Integer roomId;
+	private String location;
+	private String building;
+	private String roomNumber;
+	private String type;
+	private Integer seats;
+
+	public Room(int id, String location, String building, String roomNumber, String type, int seatCount) {
+        setId(id);
+        setLocation(location);
+		setBuilding(building);
+        setRoomNumber(roomNumber);
+        setType(type);
+        setSeatCount(seatCount);
+    }
+
+    public int getId() {
+           LOG.debug("returning the ID: " + roomId);
+        return roomId;
+    }
+    public void setId(int id) {
+        this.roomId = id;
+    }
+	 String getLocation() {
+         LOG.debug("returning the Address: " + location);
+        return location;
+    }
+    public void setLocation(String location) {
+           LOG.debug("setting the address");
+        final int max = 50;
+        final int min = 5;
+
+        if (location == null)
+        {
+            LOG.error("address must not be null");
+            throw new IllegalArgumentException("address must not be null");
+        }
+
+        if (location.isEmpty())
+        {
+            LOG.error("address must not be empty");
+            throw new IllegalArgumentException("address must not be empty");
+        }
+        if (location.length() > max || location.length() < min)
+        {
+            LOG.error("address must be between 5 and 50 chars in length");
+            throw new IllegalArgumentException("address must be between 5 and 50 chars in length");
+        }
+        LOG.debug("setting the address to: " + location);
+        this.location = location;
+    }
+    public String getBuilding() {
+         LOG.debug("returning the Address: " + building);
+        return building;
+    }
+    public void setBuilding(String building) {
+           LOG.debug("setting the address");
+        final int max = 50;
+        final int min = 5;
+
+        if (building == null)
+        {
+            LOG.error("address must not be null");
+            throw new IllegalArgumentException("address must not be null");
+        }
+
+        if (building.isEmpty())
+        {
+            LOG.error("address must not be empty");
+            throw new IllegalArgumentException("address must not be empty");
+        }
+        if (building.length() > max || building.length() < min)
+        {
+            LOG.error("address must be between 5 and 50 chars in length");
+            throw new IllegalArgumentException("address must be between 5 and 50 chars in length");
+        }
+        LOG.debug("setting the address to: " + building);
+        this.building = building;
+    }
+    public String getRoomNumber() {
+		LOG.debug("returning the Room Number: " + roomNumber);
+        return roomNumber;
+	}
+    public void setRoomNumber(String roomNumber) {
+         LOG.debug("setting the room number");
+        final int max = 5;
+        final int min = 3;
+
+        if (roomNumber == null)
+        {
+            LOG.error("room number must not be null");
+            throw new IllegalArgumentException("room number must not be null");
+        }
+
+        if (roomNumber.isEmpty())
+        {
+            LOG.error("room number must not be empty");
+            throw new IllegalArgumentException("room number must not be empty");
+        }
+        if (roomNumber.length() > max || roomNumber.length() < min)
+        {
+            LOG.error("room number must be between 3 and 5 chars in length");
+            throw new IllegalArgumentException("room number must be between 3 and 5 chars in length");
+        }
+        LOG.debug("setting the room number to: " + roomNumber);
+        this.roomNumber = roomNumber;
+    }
+    public String getType() {
+          LOG.debug("returning the Type: " + type);
+        return type;
+    }
+    public void setType(String type) {
+         LOG.debug("setting the type of the room");
+        final int max = 20;
+        final int min = 5;
+
+        if (type == null)
+        {
+            LOG.error("Type must not be null");
+            throw new IllegalArgumentException("Type must not be null");
+        }
+
+        if (type.isEmpty())
+        {
+            LOG.error("Type must not be empty");
+            throw new IllegalArgumentException("Type must not be empty");
+        }
+        if (type.length() > max || type.length() < min)
+        {
+            LOG.error("Type must be between 2 and 40 chars in length");
+            throw new IllegalArgumentException("Type must be between 2 and 40 chars in length");
+        }
+        LOG.debug("setting the Type to: " + type);
+        this.type = type;
+    }
+    public int getSeatCount() {
+        LOG.debug("returning the Seat Count: " + seats);
+        return seats;
+    }
+    public void setSeatCount(int seatCount) {
+        LOG.debug("setting the Seat Count");
+        final int max = 250;
+        final int min = 1;
+
+        if (seatCount > max || seatCount < min)
+        {
+            LOG.error("Seat Count must be between 1 and 250");
+            throw new IllegalArgumentException("Seat Count must be between 1 and 250");
+        }
+        LOG.debug("setting the seat count to: " + seatCount);
+        this.seats = seatCount;
+    }
+
 }
 
