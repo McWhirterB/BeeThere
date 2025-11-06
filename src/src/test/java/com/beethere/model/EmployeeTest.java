f4aa4a4c4fe00f51f238b66476ee07d10f0bb76b
Bryce McWhirter <bmcwhirter@rocketsoftware.com>
2025-11-06 16:40:11 -0500

    added employee test

---
+++ src/src/test/java/com/beethere/model/EmployeeTest.java
@@ -0,0 +1,112 @@
+package com.beethere.model;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+// import org.springframework.test.context.event.annotation;
+
+public class EmployeeTest {
+
+   
+    public Employee testEmployee = new Employee(
+        01, 
+        "Pallet Town",  
+        "Ash", 
+        "Ketchum",
+        "Acquistions", 
+        "Trainer"
+        );
+
+    // @Test
+    // void testAssertEquals() {
+    //     String actual = "test";
+    //     String expected = "whomp";
+    //     assertEquals(actual, expected);
+            
+    // }
+
+
+    @Test
+    void testGetId() {
+        int actual = testEmployee.getId();
+        int expected = 01;
+        assertEquals(actual, expected);
+            
+    }
+
+    @Test
+    void testGetLoc() {
+            String actual = testEmployee.getLoc();
+            String expected = "Pallet Town";
+        assertEquals(actual, expected);
+
+    }
+
+    @Test
+    void testGetFName() {
+        String actual = testEmployee.getFName();
+        String expected = "Ash";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testGetLName() {
+        String actual = testEmployee.getLName();
+        String expected = "Ketchum";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testGetDept() {
+        String actual = testEmployee.getDept();
+        String expected = "Acquistions";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testGetTitle() {
+        String actual = testEmployee.get();
+        String expected = "Ketchum";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testSetFName() {
+        testEmployee.setFName("Jesse");
+        String actual = testEmployee.getFName();
+        String expected = "Jesse";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testSetLName() {
+        testEmployee.setLName("James");
+        String actual = testEmployee.getLName();
+        String expected = "James";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testSetLoc() {
+        testEmployee.setLoc("TeamRocketHQ");
+        String actual = testEmployee.getLoc();
+        String expected = "TeamRocketHQ";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testSetDept() {
+        testEmployee.setDept("TeamRocket");
+        String actual = testEmployee.getDept();
+        String expected = "TeamRocket";
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    void testSetTitle() {
+        testEmployee.setTitle("Grunt");
+        String actual = testEmployee.getTitle();
+        String expected = "Grunt";
+        assertEquals(actual, expected);
+    }
+}
