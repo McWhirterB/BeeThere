1d194c643627727278e7aae96c1953ed40264870
dward <dward@rocketsoftware.com>
2025-12-09 15:58:54 -0500

    some calendar dialog bidness

--- frontend/src/components/HelloThree.vue
+++ frontend/src/components/HelloThree.vue
@@ -33,20 +33,88 @@
 								Add Meeting
 							</v-card-title>
 							<v-card-text>
-<!--								<v-text-field readonly variant="outlined" label="Start Time" prepend-inner-icon="mdi-clock-in">
-									<v-menu v-model="showStartTimeMenu" :close-on-content-click="false"
-													activator="parent" min-width="0">
-										<v-time-picker></v-time-picker>
-									</v-menu> 
-								</v-text-field> -->
+								<v-row>	
+									<v-col>
+										<v-text-field v-model="rsvpDate" type="date"></v-text-field>	
+									</v-col>
+								</v-row>
 								<v-row>
 									<v-col>
-										<v-time-picker>
-											<template #clock></template>
-										</v-time-picker>
+										<v-time-picker v-model="rsvpStartTime"></v-time-picker>
 									</v-col>
 									<v-col>
-										<v-time-picker></v-time-picker>
+										<v-time-picker v-model="rsvpEndTime"></v-time-picker>
+									</v-col>
+								</v-row>
+								<v-row>
+									<v-col align="center">
+										<v-card elevation="2" width="40%">
+											<v-card-text>
+												<v-row>
+													<v-col> 
+														<v-menu v-model="menu" :close-on-content-click="false" location="end">
+															<template v-slot:activator="{ props }">
+																<v-btn v-bind="props"> Add Rooms </v-btn>
+															</template>
+
+															<v-card min-width="300">
+																<v-list>
+																	<v-list-item>	
+																		Rooms
+																	</v-list-item>
+																</v-list>
+
+																<v-divider></v-divider>
+
+<!--																<v-list>
+																	<v-list-item>
+																		Room 1 type beat
+																		<v-icon icon="mdi-plus-box"></v-icon>
+																	</v-list-item>
+																	<v-list-item>
+																		Room 2 type beat
+																		<v-icon icon="mdi-plus-box"></v-icon>
+																	</v-list-item>
+																	<v-list-item>
+																		Room 3 type beat
+																		<v-icon icon="mdi-plus-box"></v-icon>
+																	</v-list-item>
+																</v-list>
+-->
+																<v-list v-for="(room, i) in rooms" :key="i">
+																	<v-list-item>
+																		<v-list-item-title v-text="room.roomNumber"></v-list-item-title>
+																		<v-icon icon="mdi-plus-box" @click="rsvp.rooms.push(room)"></v-icon>
+																	</v-list-item>
+																</v-list>
+
+																<v-card-actions>
+																	<v-spacer></v-spacer>
+
+																	<v-btn variant="text" @click="menu = false"> Cancel </v-btn>
+																	<v-btn color="primary" variant="text" @click="menu = false">
+																		Save
+																	</v-btn>
+																</v-card-actions>
+															</v-card>
+														</v-menu>
+													</v-col>
+												</v-row>
+												<v-row>
+													<v-col>
+														<v-list v-for="(room, i) in rsvp.rooms" :key="i">
+															<v-list-item v-text="room.roomNumber"></v-list-item>
+														</v-list>
+													</v-col>
+												</v-row>
+											</v-card-text>	
+										</v-card>
+									</v-col>
+								</v-row>
+								<v-row>
+									<v-col class="text-center">
+										<v-btn @click="postReservation()">Create Reservation</v-btn>
+										<v-btn @click="console.log(rsvp)">Test Reservation</v-btn>
 									</v-col>
 								</v-row>
 							</v-card-text>
@@ -58,7 +126,7 @@
 						</v-card>
 					</template>
 				</v-dialog>
-				<v-dialog v-model="reservationInfoDialog">
+				<v-dialog v-model="reservationInfoDialog" width="60%">
 					<v-card>
 						<v-card-text>
 							{{ selectedReservation?.name }}	
@@ -71,8 +139,11 @@
 </template>
 
 <script setup>
-  import { ref } from 'vue'
+  import { ref, onMounted } from 'vue'
+	import axios from 'axios'
+	import { useSnackbarStore } from  '../stores/snackbar-store.ts'
 
+	const snackbar = useSnackbarStore();
   const value = ref('')
   const events = ref([])
   const colors = [
@@ -90,7 +161,76 @@
 	const reservationInfoDialog = ref(false)
 	const selectedReservation = ref(null)
 	const showStartTimeMenu = ref(false)
+	const rsvpStartTime = ref('')
+	const rsvpEndTime = ref('')
+	const rsvpDate = ref('')
+	const rooms = ref([])
+	const menu = ref(false);
+	const roomsToAdd = ref([])
+	const rsvp = ref({
+					reservationId: 44,
+					employeeId: 128,
+					employeeName: "LuhTyrese",
+					startTime: "2025-12-10T15:00:00.000+00:00",
+					endTime: "2025-12-10T16:30:00.000+00:00",
+					rooms: [],
+				})
 
+	onMounted(() => {
+		getRooms()
+	})
+
+	function buildDateTime(baseTimestamp, timeString) {
+		const [hh, mm] = timeString.split(':').map(Number)
+
+		const date = new Date(baseTimestamp)
+
+		// Set hours + minutes from the time picker
+		date.setHours(hh)
+		date.setMinutes(mm)
+		date.setSeconds(0)
+		date.setMilliseconds(0)
+
+		return date.toISOString()
+	}
+
+	async function getRooms() {
+		try {
+				await axios.get("http://localhost:8080/rooms/", {
+															headers: {
+																"Bearer": "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRoIFNlcnZpY2UiLCJsYXN0X25hbWUiOiJEdWNrd29ydGgiLCJsb2NhdGlvbiI6IkJyYXppbCIsImlkIjoyLCJkZXBhcnRtZW50IjoiSW5mb3JtYXRpb24gVGVjaG5vbG9neSIsInRpdGxlIjoiRGV2ZWxvcGVyIiwiZmlyc3RfbmFtZSI6Iktlbm5hbiIsInN1YiI6Iktlbm5hbiBEdWNrd29ydGgiLCJpYXQiOjE3NjUzMDk2NDIsImV4cCI6MTc2NTMxMzI0Mn0.j71nzb_4DrQGMGBpfd6RniBhp3SMhr7WMXh2jMe_HTg",
+																"Access-Control-Allow-Origin": "*",	
+															},
+															responseType: "json",
+														}).then((response) => {
+															rooms.value = response.data;
+														});
+			console.log(rooms);
+		} catch (e) {
+			console.log("unable to get rooms: ", e);
+		}
+	}
+
+	async function postReservation() {
+		const startISO = buildDateTime(createStart.value, rsvpStartTime.value)
+		const endISO   = buildDateTime(createStart.value, rsvpEndTime.value)
+		rsvp.value.startTime = startISO
+		rsvp.value.endTime   = endISO
+		try {
+			await axios.post("http://localhost:8080/reservations/", rsvp.value, {
+										headers: {
+											"Bearer": "eyJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJBdXRoIFNlcnZpY2UiLCJsYXN0X25hbWUiOiJEdWNrd29ydGgiLCJsb2NhdGlvbiI6IkJyYXppbCIsImlkIjoyLCJkZXBhcnRtZW50IjoiSW5mb3JtYXRpb24gVGVjaG5vbG9neSIsInRpdGxlIjoiRGV2ZWxvcGVyIiwiZmlyc3RfbmFtZSI6Iktlbm5hbiIsInN1YiI6Iktlbm5hbiBEdWNrd29ydGgiLCJpYXQiOjE3NjUzMDk2NDIsImV4cCI6MTc2NTMxMzI0Mn0.j71nzb_4DrQGMGBpfd6RniBhp3SMhr7WMXh2jMe_HTg",
+											"Access-Control-Allow-Origin": "*",	
+										},
+										responseType: "json",
+									}).then((response) => {
+										console.log(response);
+									});	
+		} catch (error) {
+			console.error("unable to post reservation: ", error)
+		}	
+	}
+		
 	function onEventClick({ event }) {
 		console.log(event)
 		selectedReservation.value = event
@@ -101,6 +241,11 @@
 		addReservationDialog.value = true;
     const mouse = toTime(tms)
 		createStart.value = roundTime(mouse)
+		const date = new Date(createStart.value)
+		const hh = String(date.getHours()).padStart(2, '0');
+		const mm = String(date.getMinutes()).padStart(2, '0');
+		rsvpStartTime.value = `${hh}:${mm}`;
+		rsvpEndTime.value = '';
 		createEvent.value = {
 			name: `Event #${events.value.length}`,
 			color: rndElement(colors),
